3d4d0f7e47dba1d02be3da73d8eca6969127d0a4
rborghoff <raymond.borghoff@hva.nl>
2020-04-12 14:47:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/loancollector/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.loancollector
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    private val splashDelay: Long = 2500;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        goToHomeScreen();
+    }
+
+    private fun goToHomeScreen(){
+        // Use Handler to wait 1 second before opening the HomeActivity.
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, HomeActivity::class.java));
+            // Call finish, so user can not return to splash screen.
+            finish();
+
+            // Animation to fade in and fade out the SplashActivity.
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        }, splashDelay);
+    }
+}
