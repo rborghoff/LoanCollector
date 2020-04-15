3a5e15616cf67144c031af9c7dfed7572181d019
rborghoff <raymond.borghoff@hva.nl>
2020-04-15 10:34:38 +0200

    eindelijk wat bugs eruit kunnen halen die ervoor zorgde dathet project niet meer wilde starten

---
+++ app/src/main/java/com/example/loancollector/LoanDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.loancollector
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoanDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loan_detail)
+    }
+}
